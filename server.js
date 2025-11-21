5e71e4d9bc8ed8df53c9b6a2fa05bc77baf18c17
gkloja <gbscabral15@gmail.com>
2025-11-20 21:40:51 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.use(async (req, res) => {
+  try {
+    const targetUrl = "http://br2.bronxyshost.com:4009/lojagk" + req.url;
+
+    const response = await fetch(targetUrl);
+    const body = await response.text();
+
+    res.set("Content-Type", response.headers.get("content-type"));
+    res.send(body);
+
+  } catch (err) {
+    res.status(500).send("Erro ao carregar o site.");
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Proxy rodando na porta " + port));
